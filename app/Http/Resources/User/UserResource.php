6b7fd609a9ebbab1cba17971ce5e3ae4f18a0c58
Mario Gomes <markkenedy232323@gmail>
2023-03-29 17:01:43 +0000

    alterando as saidas dos resource do User

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Resources\User;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return
+         [
+            "first_name"=>$this->first_name,
+            "last_name"=>$this->last_name,
+            "email"=>$this->email,
+            "created_at"=>Carbon::make($this->created_at)->format("d/m/y")
+
+
+        ];
     }
 }
