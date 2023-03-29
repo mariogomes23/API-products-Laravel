125bc904161e4ad5a4fecb1fb59c60f948db9632
Mario Gomes <markkenedy232323@gmail>
2023-03-29 14:50:02 +0000

    Iniciando as Migracoes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+
+    }
+
+    public function index(){
+
+        echo "hello word";
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get("/",function(){
-
-    echo "ola";
-});
+Route::get("/",[UserController::class,"index"]);
